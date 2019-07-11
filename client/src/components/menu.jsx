7987026c7f78de6a366d3ef70eae126e3f7ca04c
Grant Steinke <grantjsteinke@gmail.com>
2019-07-10 18:47:05 -0600

    adds modal

--- client/src/components/menu.jsx
+++ client/src/components/menu.jsx
@@ -1,27 +1,57 @@
 import React from 'react';
 import Item from './item.jsx';
+import Modal from './modal.jsx';
 
 
 class Menu extends React.Component {
-  constructor({props}) {
-    super({props});
+  constructor(props) {
+    super(props);
     this.state = {
       showModal: false,
-      itemID: ''
+      modalData: {}
     }
     this.handleItemClick = this.handleItemClick.bind(this)
+    this.openModal = this.openModal.bind(this)
+    this.closeModal = this.closeModal.bind(this)
+  }
+  openModal() {
+    this.setState({
+        showModal: true
+    });
+  }
+
+  closeModal() {
+    this.setState({
+        showModal: false
+    });
   }
 
   handleItemClick(e) {
-    console.log(e.target.parentNode.id)
+    for (let i in this.props.menuItems){
+      if (e.target.parentNode.id === this.props.menuItems[i]._id) {
+        this.setState({modalData: this.props.menuItems[i]})
+        this.openModal()
+      }
+    }
   }
+
   render() {
     return (
-      <div className = 'menu'>
-        <ul>
-          {this.props.menuItems.map(item => <Item item = {item} handleItemClick = {this.handleItemClick}/>)}
-        </ul>
-      </div>
+      <>
+      { this.state.showModal ? <div onClick={this.closeModal} className="back-drop"></div> : null }
+        <button className="open-modal-btn" onClick={this.openModal}>Open Modal</button>
+        <Modal
+            data = {this.state.modalData}
+            className="modal"
+            show={this.state.showModal}
+            close={this.closeModal}>
+        </Modal>
+        <div className = 'menu'>
+          <ul>
+            {this.props.menuItems.map(item => <Item item = {item} handleItemClick = {this.handleItemClick}/>)}
+          </ul>
+        </div>
+      </>
     )
   }
 }
---
+++ client/src/components/modal.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const modal = (props) => {
+    return (
+        <div>
+            <div className="modal-wrapper"
+                style={{
+                    transform: props.show ? 'translateY(0vh)' : 'translateY(-100vh)',
+                    opacity: props.show ? '1' : '0'
+                }}>
+                <div className="modal-header">
+                    <h3>{props.data.name}</h3>
+                    <span className="close-modal-btn" onClick={props.close}>×</span>
+                </div>
+                <div className="modal-body">
+                    <p>
+                        {props.data.description}
+                    </p>
+                </div>
+                <div className="modal-footer">
+                    <button className="btn-cancel" onClick={props.close}>CLOSE</button>
+                    <button className="btn-continue">CONTINUE</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default modal;
