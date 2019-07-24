58fe91999facf7a49af4b2d0b9002a375597b5dc
glkapitan <galinaberger19@gmail.com>
2019-07-24 17:22:10 -0400

    added new api points

--- server/index.js
+++ server/index.js
@@ -18,6 +18,26 @@ app.use('/:menu', express.static(path.join(__dirname, '../client/dist')));
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: false }));
 
+
+app.get('/api/menu', function (req, res) {
+  const menu = parseInt(req.params.menu);
+  if (menu > 100) {
+    res.status(404).send();
+  } else {
+    MenuItem.getAllMenus().then(data => res.status(202).send(data)).catch(err => console.log(`API Error:${err}`));
+  }
+});
+
+app.post('/api/menu/', function (req, res) {
+  MenuItem.createMenu(req.body, (err, messages) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(messages);
+    }
+  });
+});
+
 app.get('/api/menu/:menu', function (req, res) {
   const menu = parseInt(req.params.menu);
   if (menu > 100) {
@@ -26,6 +46,25 @@ app.get('/api/menu/:menu', function (req, res) {
     MenuItem.getMenu({ menu }).then(data => res.status(202).send(data)).catch(err => console.log(`API Error:${err}`));
   }
 });
+
+app.put('/api/menu/:menu', function (req, res) {
+  const menu = parseInt(req.params.menu);
+  if (menu > 100) {
+    res.status(404).send();
+  } else {
+    MenuItem.updateMenu({ menu }).then(data => res.status(202).send(data)).catch(err => console.log(`API Error:${err}`));
+  }
+});
+
+app.delete('/api/menu/:menu', function (req, res) {
+  const menu = parseInt(req.params.menu);
+  if (menu > 100) {
+    res.status(404).send();
+  } else {
+    MenuItem.deleteMenu({ menu }).then(data => res.status(202).send(data)).catch(err => console.log(`API Error:${err}`));
+  }
+});
+
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
